43e0469aef6a2fddaa40d089be4bb15c8c72ba9b
cgaud <A02363037@aggies.usu.edu>
2022-03-30 19:39:55 -0600

    My Second Commit

---
+++ homework8.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+from scapy.all import DNS, DNSQR, IP, sr1, UDP
+import sys
+queryHost=sys.argv[1]
+
+dnsRequest = IP(dst='8.8.8.8')/UDP(dport=53)/DNS(rd=1,qd=DNSQR(qname=queryHost)) 
+dnsResponse = sr1(dnsRequest)
+print(dnsResponse[DNS].summary())
